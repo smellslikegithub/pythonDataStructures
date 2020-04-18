9b06100c27ddf271e6b31678ced761007650a399
smellslikegithub <ashish.m.husain@gmail.com>
2020-04-18 20:30:22 +0200

    Stack Foundation

---
+++ Main.py
@@ -0,0 +1,13 @@
+from Stack import Stack as stack
+
+
+def main():
+    myStack = stack()
+    myStack.push(100)
+    myStack.push(100)
+    myStack.push(100)
+    myStack.push(100)
+    myStack.push(100)
+
+if __name__ == "__main__":
+    main()
